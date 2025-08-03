a780ad0ff36193843413b471cdc0b9bd688e38c0
Coder. Naiem <126259025+Naiem548@users.noreply.github.com>
2025-08-04 01:57:36 +0600

    assignment-upgrade

--- problem6.js
+++ problem6.js
@@ -4,12 +4,12 @@ var startingSalary = 45000;
 //write your code here
 
 
-
 var starting = startingSalary;
 
 for (var i = 1; i <= experience; i++) {
   starting = starting += (starting * 0.05);
 }
+
 
 console.log(starting.toFixed(2));
 
